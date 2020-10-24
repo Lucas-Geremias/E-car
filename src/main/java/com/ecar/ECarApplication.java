a76fd4541dffe95280db9071730403e53e75a603
Lucas-Geremias <geremiaslucas2012@gmail.com>
2020-10-24 17:42:48 -0300

    começando o projeto

---
+++ src/main/java/com/ecar/ECarApplication.java
@@ -0,0 +1,13 @@
+package com.ecar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecar/ECarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
